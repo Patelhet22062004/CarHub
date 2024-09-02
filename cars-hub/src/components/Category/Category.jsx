150e07d78b05890a760f0aa257948275f38aa25b
Patel_Het <124024799+Patelhet22062004@users.noreply.github.com>
2024-09-02 17:54:15 +0530

    Update Category.jsx

--- cars-hub/src/components/Category/Category.jsx
+++ cars-hub/src/components/Category/Category.jsx
@@ -7,29 +7,29 @@ function Category() {
   <div className='grid md:grid-cols-7 grid-cols-3  gap-2 py-6 justify-center'>
     
     <div className="p-4 text-center w-auto hover:bg-blue-50 h-24 rounded  hover:scale-105 hover:transition-all" >
-    <img src='public/images/images/logos/honda.webp'/>
+    <img src='/images/images/logos/honda.webp'/>
 
   </div>
     <div className="p-4 text-center w-auto hover:bg-blue-50 h-24 rounded  hover:scale-105 hover:transition-all" >
-    <img src='public/images/images/logos/hyundai.webp'/>
+    <img src='/images/images/logos/hyundai.webp'/>
 
   </div>
   <div className="p-4 text-center w-auto hover:bg-blue-50 h-24 rounded  hover:scale-105 hover:transition-all" >
-  <img src='public/images/images/logos/maruti-suzuki.webp'/>
+  <img src='/images/images/logos/maruti-suzuki.webp'/>
 
   </div><div className="p-4 text-center hover:bg-blue-50 w-auto h-24 rounded  hover:scale-105 hover:transition-all" >
-  <img src='public/images/images/logos/renault.webp'/>
+  <img src='/images/images/logos/renault.webp'/>
 
   </div><div className="p-4 text-center hover:bg-blue-50 w-auto h-24 rounded  hover:scale-105 hover:transition-all" >
-  <img src='public/images/images/logos/tata.webp'/>
+  <img src='/images/images/logos/tata.webp'/>
 
   </div>
   <div className="py-4 text-center w-auto hover:bg-blue-50 h-24 rounded  hover:scale-105 hover:transition-all" >
-  <img src='public/images/images/logos/toyota.webp'/>
+  <img src='/images/images/logos/toyota.webp'/>
 
   </div>
   <div className="py-4 text-center w-auto hover:bg-blue-50 h-24 rounded hover:scale-105 hover:transition-all" >
-      <img src='public/images/images/logos/ford.webp'/>
+      <img src='/images/images/logos/ford.webp'/>
  </div>
   </div>
 </>  
